92a5dbcdec957178cb8c26d5b901bd5c32bb4631
1999ADEK <b06201018@ntu.edu.tw>
2020-03-05 14:16:50 +0800

    Move batch and iterator to util

--- train.py
+++ train.py
@@ -9,28 +9,8 @@ from models.layer_utils import *
 from optims.NoamOpt import NoamOpt
 from options.train_options import TrainOptions
 from utils.util import *
+frim utils.batch_iter import *
 
-#TODO check pad_idx
-
-class Batch:
-    "Object for holding a batch of data with mask during training."
-    def __init__(self, src, trg=None, pad=0):
-        self.src = src
-        self.src_mask = (src != pad).unsqueeze(-2)
-        if trg is not None:
-            self.trg = trg[:, :-1]
-            self.trg_y = trg[:, 1:]
-            self.trg_mask = \
-                self.make_std_mask(self.trg, pad)
-            self.ntokens = (self.trg_y != pad).data.sum().item()
-    
-    @staticmethod
-    def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & Variable(
-            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
-        return tgt_mask
 
 def run_epoch(data_iter, model, loss_compute):
     "Standard Training and Logging Function"
@@ -52,19 +32,6 @@ def run_epoch(data_iter, model, loss_compute):
             start = time.time()
             tokens = 0
     return total_loss / total_tokens
-
-global max_src_in_batch, max_tgt_in_batch
-def batch_size_fn(new, count, sofar):
-    "Keep augmenting batch and calculate total number of tokens + padding."
-    global max_src_in_batch, max_tgt_in_batch
-    if count == 1:
-        max_src_in_batch = 0
-        max_tgt_in_batch = 0
-    max_src_in_batch = max(max_src_in_batch,  len(new.Japanese))
-    max_tgt_in_batch = max(max_tgt_in_batch,  len(new.Chinese) + 2)
-    src_elements = count * max_src_in_batch
-    tgt_elements = count * max_tgt_in_batch
-    return max(src_elements, tgt_elements)
 
 class LabelSmoothing(nn.Module):
     "Implement label smoothing."
@@ -106,28 +73,6 @@ class SimpleLossCompute:
             self.opt.optimizer.zero_grad()
         return loss.item() * norm
 
-class MyIterator(data.Iterator):
-    def create_batches(self):
-        if self.train:
-            def pool(d, random_shuffler):
-                for p in data.batch(d, self.batch_size * 100):
-                    p_batch = data.batch(
-                        sorted(p, key=self.sort_key),
-                        self.batch_size, self.batch_size_fn)
-                    for b in random_shuffler(list(p_batch)):
-                        yield b
-            self.batches = pool(self.data(), self.random_shuffler)
-            
-        else:
-            self.batches = []
-            for b in data.batch(self.data(), self.batch_size,
-                                          self.batch_size_fn):
-                self.batches.append(sorted(b, key=self.sort_key))
-
-def rebatch(pad_idx, batch):
-    "Fix order in torchtext to match ours"
-    src, trg = batch.Japanese.transpose(0, 1), batch.Chinese.transpose(0, 1)
-    return Batch(src, trg, pad_idx)
 
 if __name__ == '__main__':
     # parse options
